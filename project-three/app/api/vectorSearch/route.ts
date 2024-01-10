22b2f9e770c54c5c18dd0e96a29f285ac20ae2c4
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-01-10 10:07:58 -0800

    adding try catch block

--- project-three/app/api/vectorSearch/route.ts
+++ project-three/app/api/vectorSearch/route.ts
@@ -6,36 +6,45 @@ export async function POST(req: Request) {
   const client = await mongoClientPromise;
   const dbName = "docs";
   const collectionName = "embeddingsHounder";
-  const collection = client.db(dbName).collection(collectionName);
+  try {
+    const collection = client.db(dbName).collection(collectionName);
 
-  const question = await req.text();
+    const question = await req.text();
 
-  const vectorStore = new MongoDBAtlasVectorSearch(
-    new OpenAIEmbeddings({
-      modelName: "text-embedding-ada-002",
-      stripNewLines: true,
-    }),
-    {
-      collection,
-      indexName: "default",
-      textKey: "text",
-      embeddingKey: "embedding",
-    }
-  );
+    const vectorStore = new MongoDBAtlasVectorSearch(
+      new OpenAIEmbeddings({
+        modelName: "text-embedding-ada-002",
+        stripNewLines: true,
+      }),
+      {
+        collection,
+        indexName: "default",
+        textKey: "text",
+        embeddingKey: "embedding",
+      }
+    );
 
-  const retriever = vectorStore.asRetriever({
-    searchType: "mmr",
-    searchKwargs: {
-      fetchK: 50, //number of documents to fetch
-      lambda: 0.5, // Increase this value for more diverse results
-    },
-  });
+    const retriever = vectorStore.asRetriever({
+      searchType: "mmr",
+      searchKwargs: {
+        fetchK: 50, //number of documents to fetch
+        lambda: 0.5, // Increase this value for more diverse results
+      },
+    });
 
-  const retrieverOutput = await retriever.getRelevantDocuments(question);
+    const retrieverOutput = await retriever.getRelevantDocuments(question);
 
-  // Return a response with JSON data
-  return new Response(JSON.stringify(retrieverOutput), {
-    headers: {"Content-Type": "application/json"},
-    status: 200, // or appropriate HTTP status code
-  });
+    // Return a response with JSON data
+    return new Response(JSON.stringify(retrieverOutput), {
+      headers: {"Content-Type": "application/json"},
+      status: 200, // or appropriate HTTP status code
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({message: "Internal server error", error}), {
+      headers: {"Content-Type": "application/json"},
+      status: 500, // Internal Server Error
+
+    });
+  }
 }

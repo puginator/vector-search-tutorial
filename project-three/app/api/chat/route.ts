8bf6e48bc2d2a4cfc890f6463636b1d8538e1383
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-01-15 11:28:38 -0800

    added a try catch blcok for erros

--- project-three/app/api/chat/route.ts
+++ project-three/app/api/chat/route.ts
@@ -10,59 +10,69 @@ export async function POST(req: Request) {
 
   const apiUrl = process.env.REACT_API_URL as string;
   //console.log('URL', apiUrl);
+  try {
+    const response = await fetch(`${apiUrl}/api/vectorSearch`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(currentMessageContent),
+    })
+    const text = await response.text();
 
-  const vectorSearch = await fetch(`${apiUrl}/api/vectorSearch`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(currentMessageContent),
-  })
-  .then((res) => res.text()) // Get the raw response body as a string
-  .then((text) => {
-    //console.log(text); // Log the raw response body
-    return JSON.parse(text); // Parse the JSON manually
-  });
- // const vectorSearchJson = JSON.parse(vectorSearch);
-
-  // const vectorSearchString = vectorSearch
-  //   .map((v: any) => v.pageContent)
-  //   .join("\n\n")
-  //   .replace(/\n/g, " ");
-
-
-  //console.log('vector string', vectorSearch[0].pageContent);
-
-  const TEMPLATE = `You are a very enthusiastic Hounder representative who loves to help people learn about all things Hounder! Given the following sections from the Hounder documentation, answer the questions as if you are Hounder using only that information, outputted in PDF. Please provide a detailed technical approach the questions in your response. If you are unsure and the answer is not written in the documentation, say "Sorry, I don't know how to help with that." 
+    //try to parse the repsonse as JSON
+    const vectorSearch = JSON.parse(text);
+    console.log('vector search', vectorSearch);
+    // .then((res) => res.text()) // Get the raw response body as a string
+    // .then((text) => {
+    //   //console.log(text); // Log the raw response body
+    //   return JSON.parse(text); // Parse the JSON manually
+    // });
+    // const vectorSearchJson = JSON.parse(vectorSearch);
   
-  Context sections:
-  ${vectorSearch[0].pageContent}
+    // const vectorSearchString = vectorSearch
+    //   .map((v: any) => v.pageContent)
+    //   .join("\n\n")
+    //   .replace(/\n/g, " ");
+  
+  
+    //console.log('vector string', vectorSearch[0].pageContent);
+    const TEMPLATE = `You are a very enthusiastic Hounder representative who loves to help people learn about all things Hounder! Given the following sections from the Hounder documentation, answer the questions as if you are Hounder using only that information, outputted in PDF. Please provide a detailed technical approach the questions in your response. If you are unsure and the answer is not written in the documentation, say "Sorry, I don't know how to help with that." 
+    
+    Context sections:
+    ${vectorSearch[0].pageContent}
+  
+    Question: """
+    ${currentMessageContent}
+    """
+    `;
+  
+    messages[messages.length -1].content = TEMPLATE;
+  
+    const { stream, handlers } = LangChainStream();
+  
+    const llm = new ChatOpenAI({
+      modelName: "gpt-4-1106-preview",
+      streaming: true,
+    });
+  
+    llm
+      .call(
+        (messages as Message[]).map(m =>
+          m.role == 'user'
+            ? new HumanMessage(m.content)
+            : new AIMessage(m.content),
+        ),
+        {},
+        [handlers],
+      )
+      .catch(console.error);
+  
+    return new StreamingTextResponse(stream);
+    
+  } catch (error) {
+    console.error('error fetching data', error)
+    
+  }
 
-  Question: """
-  ${currentMessageContent}
-  """
-  `;
-
-  messages[messages.length -1].content = TEMPLATE;
-
-  const { stream, handlers } = LangChainStream();
-
-  const llm = new ChatOpenAI({
-    modelName: "gpt-4-1106-preview",
-    streaming: true,
-  });
-
-  llm
-    .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error);
-
-  return new StreamingTextResponse(stream);
 }

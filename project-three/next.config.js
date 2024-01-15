9a87f45f40933fbc3fce788f067025b53abb9834
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-01-15 09:49:47 -0800

    adjust nextConfig headers

--- project-three/next.config.js
+++ project-three/next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://vector-search-tutorial.vercel.app/ https://vector-search-tutorial-l0i2l14uu-puginator.vercel.app/",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "X-Permitted-Cross-Domain-Policies",
+            value: "none",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "off",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;

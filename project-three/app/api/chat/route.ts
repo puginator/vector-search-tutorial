a313d0051e4d888582dee34d2317b607ac84f600
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-01-15 11:17:40 -0800

    cleaning up response

--- project-three/app/api/chat/route.ts
+++ project-three/app/api/chat/route.ts
@@ -25,18 +25,18 @@ export async function POST(req: Request) {
   });
  // const vectorSearchJson = JSON.parse(vectorSearch);
 
-  const vectorSearchString = vectorSearch
-    .map((v: any) => v.pageContent)
-    .join("\n\n")
-    .replace(/\n/g, " ");
+  // const vectorSearchString = vectorSearch
+  //   .map((v: any) => v.pageContent)
+  //   .join("\n\n")
+  //   .replace(/\n/g, " ");
 
 
-  //console.log('vector string', vectorSearchString);
+  //console.log('vector string', vectorSearch[0].pageContent);
 
   const TEMPLATE = `You are a very enthusiastic Hounder representative who loves to help people learn about all things Hounder! Given the following sections from the Hounder documentation, answer the questions as if you are Hounder using only that information, outputted in PDF. Please provide a detailed technical approach the questions in your response. If you are unsure and the answer is not written in the documentation, say "Sorry, I don't know how to help with that." 
   
   Context sections:
-  ${vectorSearchString}
+  ${vectorSearch[0].pageContent}
 
   Question: """
   ${currentMessageContent}

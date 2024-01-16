d09b0ba9cfaa0cd69f6b7c8d9ca5b6c7c1613c64
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-01-16 13:53:53 -0800

    removed headers in nextConfig

--- project-three/next.config.js
+++ project-three/next.config.js
@@ -1,39 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-ancestors 'self' https://vector-search-tutorial.vercel.app/ https://vector-search-tutorial-puginator.vercel.app/",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "X-Permitted-Cross-Domain-Policies",
-            value: "none",
-          },
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "off",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
